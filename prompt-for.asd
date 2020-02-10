267f04c273e8ccc0ac73db436fa75acb4d445a29
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-02-10 14:19:58 +0900

    Cleanup: Formatted.

--- prompt-for.asd
+++ prompt-for.asd
@@ -2,7 +2,7 @@
 (in-package :asdf)
 
 (defsystem :prompt-for
-  :version "2.0.1"
+  :version "2.0.2"
   :author "Shinichi Sato"
   :description "Type safe user input."
   :long-description #.(read-file-string(subpathname *load-pathname*
--- prompt-for.lisp
+++ prompt-for.lisp
@@ -1,126 +1,129 @@
 (in-package :cl-user)
-(defpackage :prompt-for(:use :cl)
-  (:export
-    #:prompt-for
-    #:do-with-prompt-input
-    ))
+
+(defpackage :prompt-for
+  (:use :cl)
+  (:export #:prompt-for #:do-with-prompt-input))
+
 (in-package :prompt-for)
 
-(defgeneric prompt-for(target &rest args))
+(defgeneric prompt-for
+    (target &rest args))
 
 (defmethod prompt-for :around (target &rest args)
-  (declare(ignore args target))
-  (LET*((*STANDARD-INPUT* 	*QUERY-IO*)
-	(*STANDARD-OUTPUT* 	*QUERY-IO*)
-	(*PRINT-ARRAY*		t)
-	(*PRINT-BASE*		10)
-	(*PRINT-CASE*		:UPCASE)
-	(*PRINT-CIRCLE*		t)
-	(*PRINT-ESCAPE*		t)
-	(*PRINT-GENSYM*		t)
-	(*PRINT-LENGTH*		nil)
-	(*PRINT-LEVEL*		nil)
-	(*PRINT-LINES*		nil)
-	(*PRINT-MISER-WIDTH*	nil)
-	(*PRINT-PRETTY*		nil)
-	(*PRINT-RADIX*		nil)
-	(*PRINT-READABLY*	nil)
-	(*PRINT-RIGHT-MARGIN*	nil))
+  (declare (ignore args target))
+  (let* ((*standard-input* *query-io*)
+         (*standard-output* *query-io*)
+         (*print-array* t)
+         (*print-base* 10)
+         (*print-case* :upcase)
+         (*print-circle* t)
+         (*print-escape* t)
+         (*print-gensym* t)
+         (*print-length* nil)
+         (*print-level* nil)
+         (*print-lines* nil)
+         (*print-miser-width* nil)
+         (*print-pretty* nil)
+         (*print-radix* nil)
+         (*print-readably* nil)
+         (*print-right-margin* nil))
     (call-next-method)))
 
-(defmacro do-with-prompt-input(((out &rest format-args)
-				(var reader))
-			       &body body)
-  `(PROG((,out *STANDARD-OUTPUT*)
-	 ,var)
-     :REC
-     (APPLY #'FORMAT T ,@format-args)
-     (FORCE-OUTPUT)
-     (HANDLER-BIND((ERROR(LAMBDA(C)
-			   (FORMAT T "~&Invalid input. ~S"C)
-			   (CLEAR-INPUT) ; ecl need especially symbol is input with unknown package specified.
-			   (GO :REC))))
-       (SETF ,var (funcall ,reader *query-io*)))
-     (CLEAR-INPUT)
-     ,@body
-     (GO :REC)))
+(defmacro do-with-prompt-input
+          (((out &rest format-args) (var reader)) &body body)
+  `(prog ((,out *standard-output*) ,var)
+    :rec
+     (apply #'format t ,@format-args)
+     (force-output)
+     (handler-bind ((error
+                      (lambda (c)
+                        (format t "~&Invalid input. ~S" c)
+                        (clear-input) ; ecl need especially symbol is input
+                                      ; with unknown package specified.
+                        (go :rec))))
+       (setf ,var (funcall ,reader *query-io*)))
+     (clear-input)
+    ,@body
+     (go :rec)))
 
-(defmethod prompt-for((target symbol)&rest args)
+(defmethod prompt-for ((target symbol) &rest args)
   "Ensure user input is type of TARGET."
-  (multiple-value-bind(reader args)(retrieve-keyword-arg :by args #'read)
-    (do-with-prompt-input((out args)
-			  (in reader))
-      (if(typep in target)
-	(return in)
-	(format out"~&~S is type of ~S, not ~S."in(type-of in)target)))))
+  (multiple-value-bind (reader args)
+      (retrieve-keyword-arg :by args #'read)
+    (do-with-prompt-input ((out args) (in reader))
+      (if (typep in target)
+          (return in)
+          (format out "~&~S is type of ~S, not ~S." in (type-of in) target)))))
 
-(defun retrieve-keyword-arg(key args &optional default)
-  (if(eq key (car args)) ; for short cut.
-    (values(second args)(cddr args))
-    (labels((rec(list)
-	      (if(endp(cdr list))
-		(values default args)
-		(if(eq key (second list))
-		  (values(third list)(progn (rplacd list (cdddr list))
-					    args))
-		  (rec(cdr list))))))
-      (rec args))))
+(defun retrieve-keyword-arg (key args &optional default)
+  (if (eq key (car args)) ; for short cut.
+      (values (second args) (cddr args))
+      (labels ((rec (list)
+                 (if (endp (cdr list))
+                     (values default args)
+                     (if (eq key (second list))
+                         (values (third list)
+                                 (progn (rplacd list (cdddr list)) args))
+                         (rec (cdr list))))))
+        (rec args))))
 
-(defmethod prompt-for((target list)&rest args)
+(defmethod prompt-for ((target list) &rest args)
   "Ensure user input is type of TARGET."
-  (multiple-value-bind(reader args)(retrieve-keyword-arg :by args #'read)
-    (do-with-prompt-input((out args)
-			  (in reader))
-      (if(typep in target)
-	(return in)
-	(format out"~&~S is type of ~S, not ~S."in(type-of in)target)))))
+  (multiple-value-bind (reader args)
+      (retrieve-keyword-arg :by args #'read)
+    (do-with-prompt-input ((out args) (in reader))
+      (if (typep in target)
+          (return in)
+          (format out "~&~S is type of ~S, not ~S." in (type-of in) target)))))
 
-(defmethod prompt-for((pred function)&rest args)
+(defmethod prompt-for ((pred function) &rest args)
   "Ensure user input satisfies PRED."
-  (multiple-value-bind(reader args)(retrieve-keyword-arg :by args #'read)
-    (do-with-prompt-input((out args)
-			  (in reader))
-      (handler-case(if(funcall pred in)
-		     (return in)
-		     (format out"~&~S is type of ~S, not satisfies ~S."
-			     in(type-of in) pred))
-	(error(condition)
-	  (format out "~&~A, breaks ~S."
-		  condition pred))))))
+  (multiple-value-bind (reader args)
+      (retrieve-keyword-arg :by args #'read)
+    (do-with-prompt-input ((out args) (in reader))
+      (handler-case
+          (if (funcall pred in)
+              (return in)
+              (format out "~&~S is type of ~S, not satisfies ~S." in
+                      (type-of in) pred))
+        (error (condition)
+          (format out "~&~A, breaks ~S." condition pred))))))
 
-(defmethod prompt-for((s (eql :secret))&rest args)
+(defmethod prompt-for ((s (eql :secret)) &rest args)
   "Get user input string silently."
-  (multiple-value-bind(reader args)(retrieve-keyword-arg :by args #'read)
-    (do-with-prompt-input((out args)
-			  (in (secret-reader reader)))
-      (if(equal "" in)
-	(format out "~&empty string is not allowed.")
-	(return in)))))
+  (multiple-value-bind (reader args)
+      (retrieve-keyword-arg :by args #'read)
+    (do-with-prompt-input ((out args) (in (secret-reader reader)))
+      (if (equal "" in)
+          (format out "~&empty string is not allowed.")
+          (return in)))))
 
 ;;; Stolen from https://stackoverflow.com/questions/39797560/common-lisp-how-to-mask-keyboard-input
-#.(or ; in order to avoid #-
-    #+sbcl
-    '(progn
-       (defun echo-off()
-	 (let((tm(sb-posix:tcgetattr sb-sys:*tty*)))
-	   (setf(sb-posix:termios-lflag tm)
-	     (logandc2(sb-posix:termios-lflag tm)sb-posix:echo))
-	   (sb-posix:tcsetattr sb-sys:*tty* sb-posix:tcsanow tm)))
-       (defun echo-on()
-	 (let((tm(sb-posix:tcgetattr sb-sys:*tty*)))
-	   (setf(sb-posix:termios-lflag tm)
-	     (logior(sb-posix:termios-lflag tm)sb-posix:echo))
-	   (sb-posix:tcsetattr sb-sys:*tty* sb-posix:tcsanow tm)))
-       (defun secret-reader(reader)
-	 (lambda(stream)
-	   (declare(ignore stream))
-	   (echo-off)
-	   (unwind-protect(progn (clear-input sb-sys:*tty*)
-				 (funcall reader sb-sys:*tty*))
-	     (echo-on)))))
 
-    ;; as default
-    (warn "PROMPT-FOR: the method specialized :SECRET is not supported in ~A"(lisp-implementation-type))
-    '(defun secret-input()
-       (error "not supported in ~A"(lisp-implementation-type)))
-    )
+#.(or ; in order to avoid #-
+      #+sbcl
+      '(progn
+        (defun echo-off ()
+          (let ((tm (sb-posix:tcgetattr sb-sys:*tty*)))
+            (setf (sb-posix:termios-lflag tm)
+                    (logandc2 (sb-posix:termios-lflag tm) sb-posix:echo))
+            (sb-posix:tcsetattr sb-sys:*tty* sb-posix:tcsanow tm)))
+        (defun echo-on ()
+          (let ((tm (sb-posix:tcgetattr sb-sys:*tty*)))
+            (setf (sb-posix:termios-lflag tm)
+                    (logior (sb-posix:termios-lflag tm) sb-posix:echo))
+            (sb-posix:tcsetattr sb-sys:*tty* sb-posix:tcsanow tm)))
+        (defun secret-reader (reader)
+          (lambda (stream)
+            (declare (ignore stream))
+            (echo-off)
+            (unwind-protect
+                (progn
+                 (clear-input sb-sys:*tty*)
+                 (funcall reader sb-sys:*tty*))
+              (echo-on)))))
+      ;; as default
+      (warn "PROMPT-FOR: the method specialized :SECRET is not supported in ~A"
+            (lisp-implementation-type))
+      '(defun secret-input ()
+         (error "not supported in ~A" (lisp-implementation-type))))
